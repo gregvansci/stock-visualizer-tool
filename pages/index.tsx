56bfe4cfc71fa1496a6d5278517cf1d0448d6e4a
gregvansci <gregvansci76@hotmail.com>
2022-06-30 15:19:34 -0700

    home page layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,48 +1,64 @@
-
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center w-full">
-      <div className="top-0 z-50 px-10 navbar">
-        <div className="navbar-start">
-          <a className="font-bold">StockCheck</a>
-        </div>
-        <div className="navbar-end">
-          <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
-            <path strokeLinecap="round" strokeLinejoin="round" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
-          </svg>
+    <div className="flex flex-col items-center justify-center border-black">
+      <div className="flex flex-col items-center min-w-full bg-primary bg-blur-sm">
+        <div className="top-0 z-50 max-w-3xl p-0 navbar">
+          <div className="navbar-start">
+            <a className="text-2xl font-bold text-black"><span className="text-palette-4">Stock</span>Check</a>
+          </div>
+          <div className="navbar-end">
+            <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="black" strokeWidth={2}>
+              <path strokeLinecap="round" strokeLinejoin="round" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+            </svg>
+          </div>
+        
         </div>
         
-      </div>
-      
-      <div className="min-w-full hero">
-        <div className="flex-row justify-between min-w-full px-20 py-20 hero-content">
-          <div className="flex flex-col w-1/2">
-            <h1 className="text-4xl font-bold">Don't chase the market</h1>
-            <h1 className="text-4xl font-bold text-gray-300">Lead it.</h1>
-            <p className="mt-4">Powering leading companies to create unique possibilities. Exponential growth is possible when creativity is unbound</p>
-            <div className="flex flex-row mt-5">
-              <button className="font-bold text-gray-300 glass btn btn-success btn-active">Open Dashboard</button>
-            </div>
-          </div>
-          <div className="border ml-1/6 mockup-window bg-base-300">
-            <div className="flex justify-center h-56 bg-base-200">
-              <img src="https://images.unsplash.com/photo-1611974789855-9c2a0a7236a3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80" alt="mockup" />
+        <div className="max-w-3xl p-0 hero" >
+          <div className="flex-row justify-between min-w-full p-0 py-16 hero-content backdrop-blur-sm">
+            <div className="flex flex-col">
+              <h1 className="text-4xl font-bold text-black">Don't chase the market</h1>
+              <h1 className="text-4xl font-bold text-black">Lead it.</h1>
+              <p className="w-1/2 mt-4 text-black">Powering leading companies to create unique possibilities. Exponential growth is possible when creativity is unbound.</p>
+              <button className="w-1/3 my-8 text-white btn btn-circle">Open Dashboard</button>
             </div>
           </div>
         </div>
       </div>
 
-      <div className="min-w-full py-20 hero bg-slate-200">
-        <div className="flex flex-col text-center">
-          <h1 className="text-4xl font-extrabold text-slate-700">
+      <div className="min-w-full py-20 bg-white hero h-96 ">
+        <div className="flex flex-col max-w-3xl text-center align-center">
+          <h1 className="text-3xl font-extrabold text-black">
             Chart the Market. Change the Game.
           </h1>
-          <p className="pt-8">Join the most powerful entities creating a richer world</p>
+          <p className="pt-8 text-black">Join the most powerful entities creating a richer world. Enrich yourself and your friends.</p>
+          <div className="flex flex-wrap justify-center mt-10 text-neutral">
+            <p>Video Placeholder</p>
+          </div>
         </div>
         
       </div>
-      <footer>
-        <p>Footer</p>
+
+      <div className="min-w-full py-20 bg-primary hero h-96">
+        <div className="flex flex-col max-w-3xl text-center align-center">
+          <h1 className="text-3xl font-extrabold text-black">
+            Market Summary
+          </h1>
+          <div className="flex flex-wrap justify-center mt-10 text-neutral">
+            <p>Graph Placeholder</p>
+          </div>
+        </div>
+      </div>
+
+      <footer className="items-center min-w-full bg-white h-36 footer text-neutral">
+        <div className="flex flex-row justify-between w-3/4 max-w-3xl m-auto">
+          <div>
+            <p>2022 StockCheck - No rights reserved</p>
+          </div>
+          <div>
+            <a href="https://github.com/gregvansci/stock-visualizer-tool" className="link link-hover" target="_blank">GitHub Repo</a>
+          </div>
+        </div>
       </footer>
     </div>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,22 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'white': '#ffffff',
+        'black': '#000000',
+        'palette-1': '#D3F6D1',
+        'palette-2': '#A7D7C5',
+        'palette-3': '#74B49B',
+        'palette-4': '#5C8D89',
+        'palette-5': '#D1EECC',
+        'primary': '#d9fcf4',
+        'secondary': '#5C8D89',
+      },
+    },
   },
-  plugins: [require("daisyui")],
+  plugins: [
+    require("daisyui"),
+    require('tailwindcss-elevation')(['responsive']),
+  ],
 }
